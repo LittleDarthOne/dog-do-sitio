309f4b6dd9dd7a705ad5d56eb0c3b81b03822c71
Jeferson de Oliveira <jef.oli@gmail.com>
2018-03-02 00:20:41 -0300

    Melhor definição visual, nas cores preto e amarelo

--- src/main/webapp/js/dog.app.js
+++ src/main/webapp/js/dog.app.js
@@ -1,5 +1,17 @@
 var app = angular.module('dog.app', ['dog.service.product']);
 
+app.controller('HeaderController', function($scope) {
+
+    $scope.resizeJumbotronBanner = function(){
+        $('#jumbotron-banner').css({
+            'height': $(window).height()
+        });
+    };
+    $scope.resizeJumbotronBanner();
+    $(window).resize($scope.resizeJumbotronBanner);
+
+});
+
 app.controller('TestBreadcrumbController', function($scope, $locale) {
     $scope.items=[
         {url: '#', label: 'Área 1'},
@@ -41,5 +53,11 @@ app.controller('ProductListController', function($scope, $locale, ProductService
             }
         });
     };
+
+    $scope.bannerHeight = $(window).height();
+    // make sure div stays full width/height on resize
+    $(window).resize(function(){
+        $scope.bannerHeight = $(window).height();
+    });
 
 });
