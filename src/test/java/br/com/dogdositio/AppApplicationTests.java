af0694ccb9263f50b83823760fb97bbae1d47132
Jeferson de Oliveira <jef.oli@gmail.com>
2018-03-06 23:18:35 -0300

    Exemplo de teste de integração

--- src/test/java/br/com/dogdositio/AppApplicationTests.java
+++ src/test/java/br/com/dogdositio/AppApplicationTests.java
@@ -1,16 +1,39 @@
 package br.com.dogdositio;
 
+import br.com.dogdositio.model.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void validProductById() {
+		Long id = 1L;
+		ResponseEntity<Product> response = restTemplate.exchange("/product/" + id, HttpMethod.GET, null, Product.class);
+		assert response.getStatusCode().equals(HttpStatus.OK);
+		assert response.getBody().getId().equals(id);
+	}
+
+	@Test
+	public void productListIsNotEmpty() {
+		ResponseEntity<List<Product>> response = restTemplate.exchange("/product", HttpMethod.GET, null, new ParameterizedTypeReference<List<Product>>() {});
+		assert response.getStatusCode().equals(HttpStatus.OK);
+		assert response.getBody().isEmpty() == false;
 	}
 
 }
